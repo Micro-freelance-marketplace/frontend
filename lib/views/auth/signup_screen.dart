de1650bb36ddf27800dd0794bf84ac9f37c179be
yonas <yonas.seyfu5@gmail.com>
2026-04-05 18:47:06 +0300

    email validation for signup

--- lib/views/auth/signup_screen.dart
+++ lib/views/auth/signup_screen.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 import '../../widgets/app_text_field.dart';
 import '../main_screen.dart';
 
-class SignupScreen extends StatelessWidget {
+class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
+
+    //final _formKey = GlobalKey<FormState>();
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();}
+
+  class _SignupScreenState extends State<SignupScreen> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -13,30 +20,54 @@ class SignupScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child: ListView(
+          child: Form(
+            key:_formKey,
+            child: ListView(
             children: [
               const AppTextField(label: 'Name'),
               const SizedBox(height: 14),
-              const AppTextField(label: 'Email'),
+              //const AppTextField(label: 'Email'),
+              AppTextField(
+             label: 'Email',
+               validator: (value) {
+             if (value == null || value.isEmpty) {
+                 return "Email is required";
+                            }
+
+                      if (!value.toLowerCase().endsWith('@aastu.edu.et')) {
+                                  return "Only @AASTU.edu.et emails allowed";
+                                                               }
+
+                                 return null;
+                                    },
+                                         ),
               const SizedBox(height: 14),
               const AppTextField(label: 'Password', obscureText: true),
               const SizedBox(height: 14),
               const AppTextField(label: 'Confirm Password', obscureText: true),
               const SizedBox(height: 22),
-              FilledButton(
-                onPressed: () {
-                  Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (_) => const MainScreen()),
-                    (route) => false,
-                  );
-                },
+              FilledButton(onPressed: () {
+                         if (_formKey.currentState!.validate()) {
+                            Navigator.pushAndRemoveUntil(
+                                      context,
+                                   MaterialPageRoute(builder: (_) => const MainScreen()),
+                                      (route) => false,
+    );
+  }
+},
+                //onPressed: () {
+                 // Navigator.pushAndRemoveUntil(
+                   // context,
+                   // MaterialPageRoute(builder: (_) => const MainScreen()),
+                    //(route) => false,
+                 // );
+                //},
                 child: const Text('Sign Up'),
               ),
             ],
           ),
         ),
       ),
-    );
+    ));
   }
 }
--- lib/widgets/app_text_field.dart
+++ lib/widgets/app_text_field.dart
@@ -4,24 +4,29 @@ class AppTextField extends StatelessWidget {
   final String label;
   final bool obscureText;
   final int maxLines;
+  final String? Function(String?)? validator;
 
   const AppTextField({
     super.key,
     required this.label,
     this.obscureText = false,
     this.maxLines = 1,
+    this.validator,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField( 
       obscureText: obscureText,
       maxLines: maxLines,
+      validator: validator, 
       decoration: InputDecoration(
         labelText: label,
         filled: true,
         fillColor: const Color(0xFF232323),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
       ),
     );
   }
